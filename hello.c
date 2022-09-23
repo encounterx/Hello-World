1ce361edd163d9afebeaef721f8186ee74a0791f
encounterx <1171701312@qq.com>
2022-09-23 16:28:12 +0800

    add hello.c file

--- hello.c
+++ hello.c
@@ -2,13 +2,13 @@
  * @Author: encounterx 1171701312@qq.com
  * @Date: 2022-09-23 15:26:27
  * @LastEditors: encounterx 1171701312@qq.com
- * @LastEditTime: 2022-09-23 15:48:13
+ * @LastEditTime: 2022-09-23 16:27:31
  * @FilePath: \undefinedd:\Project\Git_Project\Hello-World\hello.c
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 #include<stdio.h>
 
 int main(){
-    printf("Hello World!");
+    printf("Hello World! oh");
     return 0;
 }
